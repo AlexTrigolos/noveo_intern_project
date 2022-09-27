6fb0b5649337927cb320c2be2e70cd7fbf2b69ca
Alex Trigolos <alexvlg34@gmail.com>
2022-09-27 15:16:21 +0300

    admin interface (controllers, routes, views)

---
+++ app/controllers/admin/bookings_controller.rb
@@ -0,0 +1,13 @@
+class Admin::BookingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :bookings, only: [:index, :show, :update, :destroy]
+    resources :reviews, only: [:index, :show, :update, :destroy]
+    resources :rooms
+  end
   root 'pages#index'
   resources :bookings
   resources :reviews, only: [:index, :create]
   resources :rooms, only: [:index, :show]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
