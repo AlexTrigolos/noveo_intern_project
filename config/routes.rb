bf415230656a36cdbd2e761a4196130a9b590dea
Alex Trigolos <alexvlg34@gmail.com>
2022-09-27 14:59:14 +0300

    user interface routes to rooms

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   resources :bookings
   resources :reviews, only: [:index, :create]
+  resources :rooms, only: [:index, :show]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
