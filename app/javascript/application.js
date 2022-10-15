59d78a78e298c8b366657919a7fbf4c5c099eea7
Alex Trigolos <alexvlg34@gmail.com>
2022-10-16 01:20:58 +0300

    error js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,13 +5,22 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("confirm_logout").onclick = function () {
-        return confirm("Do you really want to get out?");
-    };
-    document.getElementById("confirm_zip").onclick = function () {
-        return confirm("Do you need a zip?");
-    };
-    document.getElementById("confirm_csv").onclick = function () {
-        return confirm("Do you need a csv?");
-    };
+    let logout = document.getElementById("confirm_logout");
+    if (logout !== null) {
+        logout.onclick = function () {
+            return confirm("Do you really want to get out?");
+        };
+    }
+    let zip = document.getElementById("confirm_zip");
+    if (zip !== null) {
+        zip.onclick = function () {
+            return confirm("Do you need a zip?");
+        };
+    }
+    let csv = document.getElementById("confirm_csv");
+    if (csv !== null) {
+        csv.onclick = function () {
+            return confirm("Do you need a csv?");
+        };
+    }
 });
