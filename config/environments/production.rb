6c5e23df4077363d59db218064d16a9c42b42e23
Alex Trigolos <alexvlg34@gmail.com>
2022-10-15 18:03:10 +0300

    heroku prod name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "noveo_intern_project_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'noveo_intern_project.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'noveo-intern-project.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'noveo_intern_project.herokuapp.com',
+    domain: 'noveo-intern-project.herokuapp.com',
     user_name: ENV.fetch('MAILER_USER', nil),
     password: ENV.fetch('MAILER_PASSWORD', nil),
     authentication: 'plain',
