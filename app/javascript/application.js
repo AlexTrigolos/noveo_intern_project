aa58c0d031fed39a7409dc6b39dad845fc26e9b1
Alex Trigolos <alexvlg34@gmail.com>
2022-10-15 21:09:58 +0300

    ...

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,8 +4,14 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("confirm_logout").onclick = function() { return confirm("Do you really want to get out?"); };
-    document.getElementById("confirm_zip").onclick = function() { return confirm("Do you need a zip?"); };
-    document.getElementById("confirm_csv").onclick = function() { return confirm("Do you need a csv?"); };
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("confirm_logout").onclick = function () {
+        return confirm("Do you really want to get out?");
+    };
+    document.getElementById("confirm_zip").onclick = function () {
+        return confirm("Do you need a zip?");
+    };
+    document.getElementById("confirm_csv").onclick = function () {
+        return confirm("Do you need a csv?");
+    };
 });
