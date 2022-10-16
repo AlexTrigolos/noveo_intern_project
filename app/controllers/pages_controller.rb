c69b50585a535fb8920b78bf86e4d990a315730d
Alex Trigolos <alexvlg34@gmail.com>
2022-10-16 14:57:55 +0300

    add weather api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,22 @@
 
 class PagesController < ApplicationController
   def index
-    @photos_ids = []
+    @hotel_photos = hotel_photos
+    @kelvin = 273.15
+    @lat = params[:lat] || 59.9386
+    @lon = params[:lon] || 30.3141
+    api = ENV.fetch('API_OPEN_WEATHER', nil)
+    response = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?lat=#{@lat}&lon=#{@lon}&appid=#{api}")
+    @response_body_json = ActiveSupport::JSON.decode(response.body)
+  end
+
+  private
+
+  def hotel_photos
+    photos_ids = []
     Room.includes(:room_photos).find_each do |room|
-      room.room_photos.order(created_at: :desc).limit(2).each { |el| @photos_ids.push(el.id) }
+      room.room_photos.order(created_at: :desc).limit(2).each { |el| photos_ids.push(el.id) }
     end
-    @hotel_photos = Admin::RoomPhoto.where(id: @photos_ids).limit(10)
+    Admin::RoomPhoto.where(id: photos_ids).limit(10)
   end
 end
